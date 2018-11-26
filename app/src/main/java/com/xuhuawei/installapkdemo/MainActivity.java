b2874da9828edbdc1a0a6a6c69d08c1c4f7004d1
lingdian <lingdian@meili-inc.com>
2018-11-26 14:44:12 +0800

    init

--- app/src/main/java/com/xuhuawei/installapkdemo/MainActivity.java
+++ app/src/main/java/com/xuhuawei/installapkdemo/MainActivity.java
@@ -3,10 +3,12 @@ package com.xuhuawei.installapkdemo;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
@@ -39,8 +41,10 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            File file=new File(cachePath);
-            installApk(file);
+            File cachePath=new File( Environment.getExternalStorageDirectory()+"/upgrade_apk","DTMFRecognizerKey.apk");
+            if (cachePath.exists()){
+                installApk(cachePath);
+            }
         }
     };
 
@@ -66,9 +70,9 @@ public class MainActivity extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            File cachePath=new File( Environment.getExternalStorageDirectory()+"/bga_upgrade_apk","DTMFRecognizerKey.apk");
+
             intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            Uri contentUri = FileProvider.getUriForFile(getBaseContext(), BuildConfig.APPLICATION_ID + ".fileProvider", cachePath);
+            Uri contentUri = FileProvider.getUriForFile(getBaseContext(), BuildConfig.APPLICATION_ID + ".fileProvider", apkFile);
             intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
         } else {
             intent.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive");
@@ -104,6 +108,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        switch (requestCode) {
+//            case REQUEST_CODE_APP_INSTALL:
+//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    installApk();
+//                } else {
+//                    Intent intent = new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES);
+//                    startActivityForResult(intent, GET_UNKNOWN_APP_SOURCES);
+//                }
+//                break;
+//
+//        }
+//    }
 
     private void onSettingCheckUpdate() {
 
