6efefb90f891649e45cfa8de1440b187d27bc3ec
xuhuawei <xuhw@knowbox.cn>
2018-11-26 11:10:07 +0800

    init

--- app/src/main/java/com/xuhuawei/installapkdemo/MainActivity.java
+++ app/src/main/java/com/xuhuawei/installapkdemo/MainActivity.java
@@ -18,73 +18,88 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_APP_INSTALL = 1;
     private View btn_install;
+
+
+
+    private String cachePath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_install=findViewById(R.id.btn_install);
+        btn_install = findViewById(R.id.btn_install);
         btn_install.setOnClickListener(listener);
+
+
+        cachePath = getExternalFilesDir("upgrade_apk") + File.separator + getPackageName() + ".apk";
+
     }
 
-    private View.OnClickListener listener=new View.OnClickListener() {
+    private View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                boolean hasInstallPermission = isHasInstallPermissionWithO(getBaseContext());
-                if (!hasInstallPermission) {
-                    startInstallPermissionSettingActivity(MainActivity.this);
-                    return;
-                }else{
-
-                }
-            }else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                Uri contentUri = FileProvider.getUriForFile(getBaseContext(), BuildConfig.APPLICATION_ID + ".fileProvider", new File("apk地址"));
-                intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
-            }else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                Uri contentUri = FileProvider.getUriForFile(getBaseContext(), BuildConfig.APPLICATION_ID + ".fileProvider", new File("apk地址"));
-                intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
-            }else{
-                intent.setDataAndType(Uri.fromFile(new File("apk地址")), "application/vnd.android.package-archive");
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            }
-            startActivity(intent);
+            File file=new File(cachePath);
+            installApk(file);
         }
     };
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private boolean isHasInstallPermissionWithO(Context context){
-        if (context == null){
-            return false;
+
+    private void installApk(File apkFile) {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            boolean hasInstallPermission = isHasInstallPermissionWithO();
+            if (!hasInstallPermission) {
+                startInstallPermissionSettingActivity();
+                return;
+            } else {
+                onSettingCheckUpdate();
+            }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            Uri contentUri = FileProvider.getUriForFile(getBaseContext(), BuildConfig.APPLICATION_ID + ".fileProvider", apkFile);
+            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+        } else {
+            intent.setDataAndType(Uri.fromFile(new File("apk地址")), "application/vnd.android.package-archive");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         }
-        return context.getPackageManager().canRequestPackageInstalls();
+        startActivity(intent);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private boolean isHasInstallPermissionWithO() {
+        return getPackageManager().canRequestPackageInstalls();
     }
 
     /**
      * 开启设置安装未知来源应用权限界面
-     * @param context
+     *
      */
-    @RequiresApi (api = Build.VERSION_CODES.O)
-    private void startInstallPermissionSettingActivity(Activity context) {
-        if (context == null){
-            return;
-        }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void startInstallPermissionSettingActivity() {
         Intent intent = new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES);
-        context.startActivityForResult(intent,REQUEST_CODE_APP_INSTALL);
+        startActivityForResult(intent, REQUEST_CODE_APP_INSTALL);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode== Activity.RESULT_OK ){
-            switch (requestCode){
-                REQUEST_CODE_APP_INSTALL:
-//                onSettingCheckUpdate();
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_CODE_APP_INSTALL:
+                    onSettingCheckUpdate();
+                    break;
             }
         }
+    }
+
+
+    private void onSettingCheckUpdate() {
+
     }
 }
